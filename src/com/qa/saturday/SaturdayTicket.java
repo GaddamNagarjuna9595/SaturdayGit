bdc2b702eefac7f6811997b2984f83e8177b32be
HP <HP@DESKTOP-UTC920B>
2023-04-15 11:45:59 +0530

    First Commit..........

---
+++ src/com/qa/saturday/SaturdayTicket.java
@@ -0,0 +1,32 @@
+package com.qa.saturday;
+
+public class SaturdayTicket {
+
+	public static void main(String[] args) {
+		
+		String Name="Three";
+		
+		switch (Name) {
+		case "One":
+			System.out.println("He is the One");
+			break;
+			
+		case "Two":
+			System.out.println("He is the Two");
+			break;
+			
+		case "Three":
+			System.out.println("He is the Threee");
+			break;
+			
+		case "Four":
+			System.out.println("He is the Four");
+			break;
+
+		default:
+			System.out.println("Above Number Is NoT THERE");
+			break;
+		}
+	}
+
+}
